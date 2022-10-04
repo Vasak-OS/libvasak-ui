0f73cb082c524f88149c392a32a89047aaa8ffe6
Joaquin (Pato) Decima <jdecima@teco.com.ar>
2022-10-04 12:11:07 -0300

    first commit

---
+++ js/loading.js
@@ -0,0 +1,7 @@
+function closeLoading(){
+    document.getElementById('loading').style.display = 'none';
+}
+
+function openLoading(){
+    document.getElementById('loading').style.display = null;
+}
